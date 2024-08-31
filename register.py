1c71503d6f0e00eb9e958dfd465b615e1dd88584
hanzili <hanzili0217@gmail.com>
2024-08-31 00:44:10 -0400

    fix

--- register.py
+++ register.py
@@ -96,6 +96,7 @@ def setup_driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--window-size=800,1080')
     
     driver = webdriver.Chrome(options=chrome_options)
     return driver
